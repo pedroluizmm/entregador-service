ed281e206de51180e30eadae51ba8e1302d35f40
Pedro Luiz Moreira Mariano <131912166+pedroluizmm@users.noreply.github.com>
2025-06-05 23:16:46 -0300

    Add validation annotations and enable bean validation

--- src/main/java/com/unifood/entregador/dto/AtualizarStatusEntregaRequest.java
+++ src/main/java/com/unifood/entregador/dto/AtualizarStatusEntregaRequest.java
@@ -2,9 +2,11 @@ package com.unifood.entregador.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class AtualizarStatusEntregaRequest {
+    @NotBlank
     private String status;
 }
